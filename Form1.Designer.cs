25a9bca0aff42f629bb2cc545af61c2bf1d8acf4
NguyenMaiDuyKHOA <103157403+NguyenMaiDuyKHOA@users.noreply.github.com>
2024-05-25 21:04:01 +0700

    Upload giao diện trang đăng nhập

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace QLBH_DIEN_TU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_username = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txt_passwd = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Thoat = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_DangNhap = new Guna.UI2.WinForms.Guna2Button();
+            this.ckb_password = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.SuspendLayout();
+            // 
+            // txt_username
+            // 
+            this.txt_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_username.DefaultText = "";
+            this.txt_username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_username.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txt_username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_username.Location = new System.Drawing.Point(154, 39);
+            this.txt_username.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.PasswordChar = '\0';
+            this.txt_username.PlaceholderText = "";
+            this.txt_username.SelectedText = "";
+            this.txt_username.Size = new System.Drawing.Size(328, 48);
+            this.txt_username.TabIndex = 0;
+            // 
+            // txt_passwd
+            // 
+            this.txt_passwd.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_passwd.DefaultText = "";
+            this.txt_passwd.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_passwd.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_passwd.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_passwd.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_passwd.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_passwd.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txt_passwd.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_passwd.Location = new System.Drawing.Point(154, 118);
+            this.txt_passwd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_passwd.Name = "txt_passwd";
+            this.txt_passwd.PasswordChar = '\0';
+            this.txt_passwd.PlaceholderText = "";
+            this.txt_passwd.SelectedText = "";
+            this.txt_passwd.Size = new System.Drawing.Size(328, 48);
+            this.txt_passwd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TÀI KHOẢN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MẬT KHẨU";
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Thoat.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Thoat.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_Thoat.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_Thoat.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btn_Thoat.ForeColor = System.Drawing.Color.White;
+            this.btn_Thoat.Location = new System.Drawing.Point(302, 222);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(180, 45);
+            this.btn_Thoat.TabIndex = 4;
+            this.btn_Thoat.Text = "THOÁT";
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // btn_DangNhap
+            // 
+            this.btn_DangNhap.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_DangNhap.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_DangNhap.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_DangNhap.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_DangNhap.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btn_DangNhap.ForeColor = System.Drawing.Color.White;
+            this.btn_DangNhap.Location = new System.Drawing.Point(39, 222);
+            this.btn_DangNhap.Name = "btn_DangNhap";
+            this.btn_DangNhap.Size = new System.Drawing.Size(180, 45);
+            this.btn_DangNhap.TabIndex = 5;
+            this.btn_DangNhap.Text = "ĐĂNG NHẬP";
+            this.btn_DangNhap.Click += new System.EventHandler(this.btn_DangNhap_Click);
+            // 
+            // ckb_password
+            // 
+            this.ckb_password.AutoSize = true;
+            this.ckb_password.CheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.ckb_password.CheckedState.BorderRadius = 10;
+            this.ckb_password.CheckedState.BorderThickness = 0;
+            this.ckb_password.CheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.ckb_password.CheckMarkColor = System.Drawing.Color.Black;
+            this.ckb_password.Font = new System.Drawing.Font("Calibri", 10.2F);
+            this.ckb_password.ForeColor = System.Drawing.Color.Black;
+            this.ckb_password.Location = new System.Drawing.Point(154, 183);
+            this.ckb_password.Name = "ckb_password";
+            this.ckb_password.Size = new System.Drawing.Size(157, 25);
+            this.ckb_password.TabIndex = 6;
+            this.ckb_password.Text = "Hiển thị mật khẩu";
+            this.ckb_password.UncheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.ckb_password.UncheckedState.BorderRadius = 2;
+            this.ckb_password.UncheckedState.BorderThickness = 0;
+            this.ckb_password.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.ckb_password.CheckedChanged += new System.EventHandler(this.ckb_passwd_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 291);
+            this.Controls.Add(this.ckb_password);
+            this.Controls.Add(this.btn_DangNhap);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_passwd);
+            this.Controls.Add(this.txt_username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox txt_username;
+        private Guna.UI2.WinForms.Guna2TextBox txt_passwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2Button btn_Thoat;
+        private Guna.UI2.WinForms.Guna2Button btn_DangNhap;
+        private Guna.UI2.WinForms.Guna2CheckBox ckb_password;
+    }
+}
+
