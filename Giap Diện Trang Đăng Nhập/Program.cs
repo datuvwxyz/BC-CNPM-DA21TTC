76236408744a177734985259061fcd1cce85b080
NguyenMaiDuyKHOA <nguyenkhoahmt789@gmail.com>
2024-05-31 12:41:21 +0700

    Update giao dien trang dang nhap

---
+++ Giap Diện Trang Đăng Nhập/Program.cs
@@ -0,0 +1,23 @@
+﻿using QLBH_DIEN_TU.UC_control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLBH_DIEN_TU
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
